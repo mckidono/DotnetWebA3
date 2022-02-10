9fad9624c137992fa3bf18015ce9a9c4e5fa9a15
YellowstoneApp <mckidonno@outlook.com>
2022-02-09 20:30:18 -0600

    putting id's in label for the java & creating java

---
+++ js/balloons.js
@@ -0,0 +1,36 @@
+﻿$('document').ready(function(){
+
+$('label#red').on("mouseover",turnRed);
+$('label#green').on("mouseover",turnGreen);
+$('label#blue').on("mouseover",turnBlue);
+
+});
+
+function turnRed(){
+    $('h1#birthday').css('color','#f51000');
+}
+function turnGreen(){
+    $('h1#birthday').css('color','#189100');
+}
+function turnBlue(){
+    $('h1#birthday').css('color','#0081d6');
+}
+
+
+$(function(){
+    $('#birthday').pickadate({ format: 'mmmm, d' });
+
+    // uncheck all checkboxes (FireFox)
+    $('.form-check-input').each(function () {
+        $(this).prop('checked', false);
+    });
+    // event listener for check/uncheck
+    $('.form-check-input').on('change', function () {
+        // make the image visible
+        $('#' + this.id + 'Img').css('visibility', 'visible')
+        // animate balloon in/out based on checkbox
+        $(this).is(':checked') ?
+         $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
+         $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+    });
+});
